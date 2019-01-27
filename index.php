ce1e26553d3b207f8feb75fddcba8e5314a2f8d8
Andrew Harkins <ExtremelyExceeding@gmail.com>
2019-01-26 20:24:29 -0600

    Bootstrap4 PHP Create in mysql
    Create row in mysql db. Check if the name is currently being used if it is send a bootstrap alert saying already in USE Send bootstrap alert if no fields are filled. Send echo if a field if not filled out

--- index.php
+++ index.php
@@ -45,7 +45,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
               $errormsg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
               <button type="button" class="close" data-dismiss="alert" aria-label="Close" aria-hidden="true">
               &times;</button>Name has <b>ALREADY</b> been <u>used</u>!<br>'. $namesafe .'</div>';
-              //die("die");
         }
     } else {
         $errormsg = '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" ' .
@@ -53,7 +52,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
             'All fields are required to continue</div>';
     }
 }
-//else { echo "ELSE on btn click "; }
 
 mysqli_close($connection);
 ?>
@@ -118,6 +116,5 @@ mysqli_close($connection);
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <!--<script></script>-->
 </body>
 </html>
